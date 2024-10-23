c5268178abf020394702b6ca27bd2f6a7acf8c4b
rdvisiontech <hr@rdvision.tech>
2024-10-23 17:03:33 +0530

    ticket house api changed and added date picker to pick date form tto till date to see the data

--- src/components/TicketHouse.jsx
+++ src/components/TicketHouse.jsx
@@ -25,35 +25,35 @@ const TicketHouse = () => {
         fetchTicketDataByStatus()
         fetchTicketCountsByCountry()
         fetchSlaesByCountry()
-    }, [])
+    }, [startDate])
 
     const fetchTicketDataByStatus = async () => {
-        const response = await axiosInstance.get('/third_party_api/ticket/ticketswithstatus')
+        const response = await axiosInstance.get(`${startDate ? `/third_party_api/ticket/ticketswithstatus/${startDate}` : `/third_party_api/ticket/ticketswithstatus`}`)
         setTicketData(response.data.ticketCounts)
     }
 
     const fetchTicketCountsByCountry = async () => {
-        const response = await axiosInstance.get("/third_party_api/ticket/ticketsCountByCountry")
+        const response = await axiosInstance.get(`${startDate ? `/third_party_api/ticket/ticketsCountByCountry/${startDate}` : `/third_party_api/ticket/ticketsCountByCountry`}`)
         setTicketDataByCountry(response.data)
     }
 
     const fetchSlaesByCountry = async () => {
-        const response = await axiosInstance.get('/third_party_api/ticket/salesByCountry')
+        const response = await axiosInstance.get(`${startDate ? `/third_party_api/ticket/salesByCountry/${startDate}` : `/third_party_api/ticket/salesByCountry`}`)
         const result = response.data;
 
-        // Assuming the response is an array of arrays like [["US", 30], ["MY", 1], ...]
-        const countriesList = result.map(item => item[0]); // Extract the first element from each subarray (countries)
-        const dataList = result.map(item => item[1]); // Extract the second element from each subarray (data counts)
+
+        const countriesList = result.map(item => item[0]);
+        const dataList = result.map(item => item[1]);
 
         setCpuntryList(countriesList);
         setSaleCount(dataList);
     }
 
     const funnelData = ticketData.map(ticket => {
-        const key = Object.keys(ticket)[0]; // Get the first key
-        const value = ticket[key]; // Get the corresponding value
+        const key = Object.keys(ticket)[0];
+        const value = ticket[key];
         return {
-            name: key,
+            name: key==="totalTickets"?"All":key,
             y: value
         };
     }).filter(item => item.y > 0);
@@ -124,23 +124,24 @@ const TicketHouse = () => {
             }
         ]
     };
-
+    console.log(startDate)
     return (
         <Container fluid className="p-0" style={{ height: "100vh", overflow: "hidden" }}>
-            <div className="d-flex justify-content-between align-items-center p-3 bg-light">
-                <h5>Current month lead conversion rate</h5>
-                <div className="d-flex">
-                    <DatePicker
-                        selected={startDate}
-                        onChange={(date) => setStartDate(date)}
-                        placeholderText="Start date"
-                    />{" "}
-                    till{" "}
-                    <DatePicker
-                        selected={endDate}
-                        onChange={(date) => setEndDate(date)}
-                        placeholderText="Due date"
-                    />
+            <div className="d-flex justify-content-between align-items-center p-3 ">
+                <h5>Leads and status info</h5>
+                <div className="d-flex justify-content-center w-50">
+                    <button  className={`${startDate?"bg-primary":"bg-success"}  m-2 rounded`} style={{height:"40px"}} onClick={() => setStartDate(null)}>All</button>
+                    <div>
+                        <input
+                            type="date"
+                            className="bg-white text-black p-1 m-2 rounded"
+                            value={startDate}
+                            onChange={(e) => setStartDate(e.target.value)}
+                            placeholderText="Start date"
+                            dateFormat="yyyy-MM-dd" // Customize the date format here
+                        />
+                    </div>
+                  
                 </div>
             </div>
 
@@ -148,13 +149,13 @@ const TicketHouse = () => {
                 {/* First Column with Funnel Chart */}
                 <Col lg={6} md={6} sm={12} className="d-flex flex-column p-3">
                     <div>
-                        <h6>Funnel Chart</h6>
+                        <h6>Tickets vs status </h6>
                         <div style={{ flex: 1 }}>
                             <HighchartsReact highcharts={Highcharts} options={funnelOptions} />
                         </div>
                     </div>
 
-                    <div className="container mt-3" style={{ marginTop: "100px" }}>
+                    <div className="container " style={{ marginTop: "100px" }}>
                         <div className="row">
                             {/* # of lost leads */}
                             <div className="col-md-6 mb-4">
@@ -209,11 +210,11 @@ const TicketHouse = () => {
 
                 {/* Second Column with Pie Chart and Bar Chart */}
                 <Col lg={6} md={6} sm={12} className="d-flex flex-column p-3">
-                    <h6>Pie Chart</h6>
+                    <h6>Tickets vs country</h6>
                     <div style={{ flex: 1 }}>
                         <HighchartsReact highcharts={Highcharts} options={pieOptions} />
                     </div>
-                    <h6>Bar Chart</h6>
+                    <h6>Sales vs country</h6>
                     <div style={{ flex: 1 }}>
                         <HighchartsReact highcharts={Highcharts} options={barOptions} />
                     </div>
